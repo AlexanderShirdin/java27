89da0cb7d661c84f64d28b2db853455545621560
AlexanderShirdin <cpt.blak@gmail.com>
2022-02-02 00:04:30 +0300

    Создана рандомная переменная в классе Koloda

--- src/lesson8/blackjack/Main.java
+++ src/lesson8/blackjack/Main.java
@@ -3,6 +3,7 @@ package lesson8.blackjack;
 public class Main {
     public static void main(String[] args) {
         Koloda koloda = new Koloda();
-        System.out.println(koloda.randomCard());
+        Card card = koloda.randomCard();
+        System.out.println(card.getName() + " (" + card.getValue() + ")");
     }
 }
