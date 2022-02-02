6a428adef8ec8edd84507f90876770c7cbe7b007
AlexanderShirdin <cpt.blak@gmail.com>
2022-02-02 23:50:38 +0300

    Урок 9

---
+++ src/lesson9/Dog.java
@@ -0,0 +1,22 @@
+package lesson9;
+
+public class Dog extends Animal {
+    public Dog() {
+
+    }
+    public Dog(String name, int age) {
+        // вариант 1 (присвоение полей к родительскому классу)
+//        this.setAge(age);
+//        this.setName(name);
+
+        // вариант 2 (смена модификатора доступа в родительсвком классе)
+//        this.name = name;
+
+        //вариант 3 (с помощью родительского конструктора)
+        super(name, age);
+    }
+
+    public void voice() {
+        System.out.println("Гав-нав");
+    }
+}
