e4e7d858d5880b8d71732b8695e71710c823c489
AlexanderShirdin <cpt.blak@gmail.com>
2022-02-12 20:19:30 +0300

    Урок 12

--- src/lesson11/Main1.java
+++ src/lesson11/Main1.java
@@ -1,6 +1,7 @@
 package lesson11;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main1 {
     public static void main(String[] args) {
@@ -8,8 +9,23 @@ public class Main1 {
         texts.add("111");
         texts.add("222");
         texts.add("333");
-        texts.add(0,"444");
-        texts.set(0,"!!!");
-        System.out.println(texts);
+        texts.add(0, "444");
+        texts.set(0, "!!!");
+
+        //способ прохода по коллекции:
+        // 1 - for (только где есть индексы)
+        for (int i = 0; i < texts.size(); i++) {
+            System.out.println(texts.get(i));
+        }
+
+        // 2 - for each (для любой коллекции)
+        for (String s : texts) {
+            System.out.println(s);
+        }
+
+        // 3 - с помощью итератора (для любой коллекции)
+        for (Iterator<String> iterator = texts.iterator(); iterator.hasNext();){
+            System.out.println(iterator.next());
+        }
     }
 }
